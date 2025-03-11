ee46f4ac5a8c23e2c31a9ad6fe55e8fffe645c33
r-happpy <hr325913@gmail.com>
2025-03-11 15:54:02 +0900

    first

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local keymap = vim.keymap
+keymap.set("n", "<C-f>", ":Telescope find_files<Return>")
+keymap.set("n", "<C-b>n", ":BufferLineCycleNext<Return>")
+keymap.set("n", "<C-b>p", ":BufferLineCyclePrev<Return>")
+keymap.set("n", "<C-b>d", ":BufferLineCloseOthers<Return>")
+keymap.set("n", "<gd>", "<cmd>Telescope lsp_definitions<CR>", { noremap = true, silent = true })
---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = function()
+      LazyVim.cmp.actions.ai_accept = function()
+        if require("copilot.suggestion").is_visible() then
+          LazyVim.create_undo()
+          require("copilot.suggestion").accept()
+          return true
+        end
+      end
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    opts = {},
+    config = function(_, opts)
+      local copilot_cmp = require("copilot_cmp")
+      copilot_cmp.setup(opts)
+      -- attach cmp source whenever copilot attaches
+      -- fixes lazy-loading issues with the copilot cmp source
+      LazyVim.lsp.on_attach(function()
+        copilot_cmp._on_insert_enter({})
+      end, "copilot")
+    end,
+    specs = {
+      {
+        "hrsh7th/nvim-cmp",
+        optional = true,
+        ---@param opts cmp.ConfigSchema
+        opts = function(_, opts)
+          table.insert(opts.sources, 1, {
+            name = "copilot",
+            group_index = 1,
+            priority = 100,
+          })
+        end,
+      },
+    },
+  },
+  { "giuxtaposition/blink-cmp-copilot" },
+}
