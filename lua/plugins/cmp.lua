54c035075ca4f54efb998cb10c97f0101ed25a95
root <root@rei>
2025-03-18 23:00:03 +0900

    enable copilot

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,41 +1,36 @@
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    priority = 100, -- Give it high priority to load early
-    config = function()
-      local cmp = require("cmp")
-      cmp.setup({
-        -- Your cmp configuration here
-        -- For example:
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "buffer" },
-          { name = "path" },
-          -- Don't add copilot source here, it will be added by copilot-cmp
+    {
+        "hrsh7th/nvim-cmp",
+        priority = 100, -- Give it high priority to load early
+        config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+                -- Your cmp configuration here
+                -- For example:
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "buffer" },
+                    { name = "path" },
+                    -- Don't add copilot source here, it will be added by copilot-cmp
+                },
+                mapping = cmp.mapping.preset.insert({
+                    -- Your key mappings
+                }),
+                -- Other settings
+            })
+        end,
+        opts = function(_, opts)
+            table.insert(opts.sources, 1, {
+                name = "copilot",
+                group_index = 1,
+                priority = 100,
+            })
+        end,
+        dependencies = {
+            -- Add cmp dependencies if any
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-nvim-lsp",
         },
-        mapping = cmp.mapping.preset.insert({
-          -- Your key mappings
-        }),
-        -- Other settings
-      })
-    end,
-    dependencies = {
-      -- Add cmp dependencies if any
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-nvim-lsp",
     },
-  },
-
-  -- Then load copilot.lua before copilot-cmp
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        -- Your copilot settings
-      })
-    end,
-  },
 }
