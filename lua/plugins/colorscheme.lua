496189d2d8ea92eea70d38600aff7433fcacdbf0
r-happy <hr325913@gmail.com>
2025-03-20 21:19:16 +0900

    onedark

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,6 @@
 return {
-  "maxmx03/solarized.nvim",
-  lazy = false,
-  priority = 1000,
-  ---@type solarized.config
-  opts = {},
-  config = function(_, opts)
-    vim.o.termguicolors = true
-    vim.o.background = "dark"
-    require("solarized").setup(opts)
-    vim.cmd.colorscheme("solarized")
-  end,
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
+  },
 }
