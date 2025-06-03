75897a668b1678fc53bbc1e4d9d5aa13f5e238cf
r-happy <hr325913@gmail.com>
2025-06-03 09:28:45 +0900

    change one dark

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -19,10 +19,26 @@
 -- }
 -- lua/plugins/rose-pine.lua
 
+-- return {
+--   "rose-pine/neovim",
+--   name = "rose-pine",
+--   config = function()
+--     vim.cmd("colorscheme rose-pine")
+--   end,
+-- }
+
+-- Using Lazy
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  config = function()
-    vim.cmd("colorscheme rose-pine")
-  end,
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("onedark").setup({
+        style = "warm",
+      })
+
+      -- Enable theme
+      require("onedark").load()
+    end,
+  },
 }
