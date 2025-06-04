8d77deb4c7882d49a7e71dfc218efc05404afd36
r-happy <hr325913@gmail.com>
2025-06-04 09:49:47 +0900

    change deep

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -34,7 +34,7 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require("onedark").setup({
-        style = "warm",
+        style = "deep",
       })
 
       -- Enable theme
