f1b8c618e1de14a0f9801fe44077dc3ef83e8c47
r-happy <hr325913@gmail.com>
2026-02-23 17:47:08 +0900

    enable gitsigns

--- lua/plugins/gitsign.lua
+++ lua/plugins/gitsign.lua
@@ -1,17 +1,17 @@
 return {
-    -- {
-    --     "lewis6991/gitsigns.nvim",
-    --     event = { "BufReadPre", "BufNewFile" },
-    --     opts = {
-    --         signs = {
-    --             add = { text = "▎" },
-    --             change = { text = "▎" },
-    --             delete = { text = "" },
-    --             topdelete = { text = "" },
-    --             changedelete = { text = "▎" },
-    --             untracked = { text = "▎" },
-    --         },
-    --         signcolumn = true,
-    --     },
-    -- },
+	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			signs = {
+				add = { text = "▎" },
+				change = { text = "▎" },
+				delete = { text = "" },
+				topdelete = { text = "" },
+				changedelete = { text = "▎" },
+				untracked = { text = "▎" },
+			},
+			signcolumn = true,
+		},
+	},
 }
